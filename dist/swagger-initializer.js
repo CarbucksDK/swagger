7c1b11fb548347404203f9dfd5b772a91a8cd5c3
Mike <>
2022-04-02 16:05:02 +0200

    Added more APIs

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -4,7 +4,9 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     urls: [
-        {url: "https://login.api.carbucks.io/swagger.yaml", name: "Login"}
+        {url: "https://login.api.carbucks.io/swagger.yaml", name: "Login"},
+        {url: "https://user.api.carbucks.io/swagger.yaml", name: "User"},
+        {url: "https://order.api.carbucks.io/swagger.yaml", name: "Order"}
     ],
     dom_id: '#swagger-ui',
     deepLinking: true,
