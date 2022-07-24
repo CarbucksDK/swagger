337601d63d4e368e988de233b7dcaaa258d83777
Mike <>
2022-07-24 21:52:48 +0200

    Added forsi

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -10,7 +10,8 @@ window.onload = function() {
         {url: "https://login.api.carbucks.io/swagger.yaml", name: "Login"},
         {url: "https://message.api.carbucks.io/swagger.yaml", name: "Message"},
         {url: "https://user.api.carbucks.io/swagger.yaml", name: "User"},
-        {url: "https://order.api.carbucks.io/swagger.yaml", name: "Order"}
+        {url: "https://order.api.carbucks.io/swagger.yaml", name: "Order"},
+        {url: "https://forsi.api.carbucks.io/swagger.yaml", name: "Forsi"}
     ],
     dom_id: '#swagger-ui',
     deepLinking: true,
